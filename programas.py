1a11748f87dd0691d5ce8ce12f5cfc0a10e459a1
Pedro Henrique <henriquevic012@gmail.com>
2021-04-11 07:20:18 -0300

    Atualizando as classes

--- programas.py
+++ programas.py
@@ -1,58 +1,39 @@
+class Programa:
 
-class Filme:
-	
-	def __init__(self, nome, ano, duracao):
-		self.__nome = nome.title()
+	def __init__(self, nome, ano):
+		self._nome = nome.title()
 		self.ano = ano
-		self.duracao = duracao
-		self.__likes = 0
+		self._likes = 0
 
-	@property
-	def nome(self):
-		return self.__nome
+		@property
+		def nome(self):
+			return self._nome
 
-	@nome.setter
-	def nome(self, novo_nome):
-		self.__nome = novo_nome.title()
+		@nome.setter
+		def nome(self, novo_nome):
+			self._nome = novo_nome.title()
 
-	@property
-	def likes(self):
-		return self.__likes
+		@property
+		def likes(self):
+			return self._likes
 
-	def dar_like(self):
-		self.__likes += 1
+		def dar_like(self):
+			self._likes += 1
 
-	
+	class Filme(Programa):
+		
+		def __init__(self, nome, ano, duracao):
+			#Herda herança da classe mãe e repassa os atributos recebidos
+			super().__init__(nome, ano)
+			self.duracao = duracao
 
-class Serie:
-	
-	def __init__(self, nome, ano, temporadas):
-		self.__nome = nome.title()
-		self.ano = ano
-		self.temporadas = temporadas
-		self.__likes = 0
+	class Serie(Programa):
+		
+		def __init__(self, nome, ano, temporadas):
+			super().__init__(nome, ano)
+			self.temporadas = temporadas
 
-	@property
-	def nome(self):
-		return self.__nome
-
-	@nome.setter
-	def nome(self, novo_nome):
-		self.__nome = novo_nome.title()
-
-	@property
-	def likes(self):
-		return self.__likes
-
-	def dar_like(self):
-		self.__likes += 1
 
 filme = Filme('homem aranha', 2002, 120)
-print(filme.nome)
-filme.nome = 'Titan'
-print(filme.nome)
-
-serie = Serie('super natural', 2002, 3)
-print(serie.nome)
-serie.nome = 'terra nova'
-print(serie.nome)
+filme.dar_like()
+print(f'{filme.nome} - {filme.duracao} : {filme.likes}')
