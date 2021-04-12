9f14ef42f04a5def8414991153ef53b8112ce4a7
Pedro Henrique <henriquevic012@gmail.com>
2021-04-12 18:12:05 -0300

    Concertando as classes e subclasses (identação)

--- programas.py
+++ programas.py
@@ -5,35 +5,33 @@ class Programa:
 		self.ano = ano
 		self._likes = 0
 
-		@property
-		def nome(self):
-			return self._nome
+	@property
+	def nome(self):
+		return self._nome
 
-		@nome.setter
-		def nome(self, novo_nome):
-			self._nome = novo_nome.title()
+	@nome.setter
+	def nome(self, novo_nome):
+		self._nome = novo_nome.title()
 
-		@property
-		def likes(self):
-			return self._likes
+	@property
+	def likes(self):
+		return self._likes
 
-		def dar_like(self):
-			self._likes += 1
+	def dar_like(self):
+		self._likes += 1
 
-	class Filme(Programa):
-		
-		def __init__(self, nome, ano, duracao):
-			#Herda herança da classe mãe e repassa os atributos recebidos
-			super().__init__(nome, ano)
-			self.duracao = duracao
+class Filme(Programa):
+	def __init__(self, nome, ano, duracao):
+		#Herda herança da classe mãe e repassa os atributos recebidos
+		super().__init__(nome, ano)
+		self.duracao = duracao
 
-	class Serie(Programa):
-		
-		def __init__(self, nome, ano, temporadas):
-			super().__init__(nome, ano)
-			self.temporadas = temporadas
+class Serie(Programa):
+	
+	def __init__(self, nome, ano, temporadas):
+		super().__init__(nome, ano)
+		self.temporadas = temporadas
 
-
-filme = Filme('homem aranha', 2002, 120)
+'''filme = Filme('homem aranha', 2002, 120)
 filme.dar_like()
-print(f'{filme.nome} - {filme.duracao} : {filme.likes}')
+print(f'{filme.nome} - {filme.duracao} : {filme.likes}')'''
